c39308d6c3f75454d25a5916154c360398fd9f89
YukiToida <yuki.toidax@gmail.com>
2019-03-12 13:16:26 +0900

    refactor package

--- _mockgen.sh
+++ _mockgen.sh
@@ -1,7 +1,4 @@
 #!/bin/sh
 
-# MicroServices
-mockgen go-bff/bff/domain/entities/entity_notify Service > bff/domain/entities/entity_notify/mock_entity_notify/mock_notify_service.go
-
 # UseCase
 mockgen go-bff/bff/application/usecase/usecase_email UseCase > bff/application/usecase/usecase_email/mock_usecase_email/mock_email.go
